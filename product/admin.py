81580bf514ba9e7e3931dde613ef31b05b5f2fec
miracziyaalev <miracziyaalev@gmail.com>
2021-05-18 14:11:25 +0300

    Category - Product
    i. Kategori ve buna ait ürün ilişkisi yapıldı

--- product/admin.py
+++ product/admin.py
@@ -1,11 +1,17 @@
 
 from django.contrib import admin
 
-from product.models import Category
+from product.models import Category, Product
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     list_filter = ['status']
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'price', 'gear', 'number_of_seats']
+    list_filter = ['category', 'status']
+
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- product/models.py
+++ product/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Category(models.Model):
+class Category (models.Model):
     STATUS = (
         ('True', 'Evet'),
         ('False', 'Hayır'),
@@ -13,6 +13,37 @@ class Category(models.Model):
     status = models.CharField(max_length=10, choices= STATUS)
     slug = models.SlugField()
     parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Product(models.Model):
+    STATUS = (
+            ('True', 'Evet'),
+            ('False', 'Hayır'),
+        )
+
+    GEAR = (
+        ('Automatic', 'Otomatik'),
+        ('Manuel', 'Manuel'),
+    )
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=255)
+    keywords = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    status = models.CharField(max_length=10, choices=STATUS)
+    price = models.FloatField()
+    gear = models.CharField(max_length=10, choices=GEAR)
+    type = models.CharField(max_length=30)
+    number_of_seats = models.IntegerField()
+    engine = models.FloatField()
+    amount = models.IntegerField()
+    detail = models.TextField()
+    slug = models.SlugField()
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
