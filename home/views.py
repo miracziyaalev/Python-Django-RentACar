6b11b6e1fd1ecdd7d77666b4226f48c7d986a4f8
miracziyaalev <miracziyaalev@gmail.com>
2021-05-19 11:43:53 +0300

    settings
    i. Ayarlar tablosu uluşturuldu. Admin paneli ile erişim sağlandı.
    
    ii. Dinamik açıklama, başlık, favicon vs. eklendi

--- home/views.py
+++ home/views.py
@@ -2,7 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from home.models import Setting
+
+
 def index(request):
-    text = 'Merhaba Djangoss'
-    context = {'text': text}
+    setting = Setting.objects.get()
+    context = {'setting': setting}
     return render(request, 'index.html', context)
