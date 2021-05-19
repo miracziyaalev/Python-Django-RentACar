1b94b90be7d50ed3e42fb364f351703ac0b1c241
miracziyaalev <miracziyaalev@gmail.com>
2021-05-19 19:44:55 +0300

    Mesaj Bırakma
    i. İletişim sayfasına mesaj bırakma bölümü eklendi.

--- home/models.py
+++ home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
+from django.forms import ModelForm, TextInput, Textarea
 
 # Create your models here.
 class Setting(models.Model):
@@ -33,3 +34,34 @@ class Setting(models.Model):
 
     def __str__(self):
         return self.title
+
+class ContactFormMessage (models.Model):
+    STATUS = (
+        ('New', 'New'),
+        ('Read', 'Read'),
+        ('Closed', 'Closed'),
+    )
+
+    name = models.CharField(blank=True, max_length=30)
+    email = models.CharField(blank=True, max_length=50)
+    subject = models.CharField(blank=True, max_length=50)
+    message = models.CharField(blank=True, max_length=255)
+    status = models.CharField(max_length=10, choices= STATUS, default='New')
+    ip = models.CharField(blank=True, max_length=20)
+    note = models.CharField(blank=True, max_length=100)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class ContactFormu(ModelForm):
+    class Meta:
+        model = ContactFormMessage
+        fields = ['name', 'email', 'subject', 'message']
+        widgets = {
+            'name' : TextInput(attrs = {'class': 'input', 'placeholder' : 'Ad & Soyad'}),
+            'subject': TextInput(attrs={'class': 'input', 'placeholder': 'Konu'}),
+            'email': TextInput(attrs={'class': 'input', 'placeholder': 'Email Adres'}),
+            'message': Textarea(attrs={'class': 'input', 'placeholder': 'Mesajınız..'}),
+        }
--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
-from django.http import HttpResponse
+from django.core.checks import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import messages
 
 # Create your views here.
-from home.models import Setting
+from home.models import Setting, ContactFormu, ContactFormMessage
 
 
 def index(request):
@@ -16,6 +18,21 @@ def hakkimizda(request):
     return render(request, 'hakkimizda.html', context)
 
 def iletisim(request):
+
+    if request.method == 'POST':
+        form = ContactFormu(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()
+            data.name = form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request, "Mesajınız başarı ile gönderilmiştir.")
+            return HttpResponseRedirect ('/iletisim')
+        
     setting = Setting.objects.get()
-    context = {'setting': setting, 'page': 'iletisim'}
+    form = ContactFormu()
+    context={'setting':setting, 'form':form}
     return render(request, 'iletisim.html', context)
