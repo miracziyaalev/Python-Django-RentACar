4edb8782941a4c923bc8521ae4904105a85df7cc
miracziyaalev <miracziyaalev@gmail.com>
2021-05-19 09:28:46 +0300

    admin panel - upload
    i. Upload edilen gorsellerin urun yapisinda listelenmesi.
    
    ii. Upload edilen gorsellerin kategori yapisinda listelenmesi

--- product/admin.py
+++ product/admin.py
@@ -1,17 +1,32 @@
 
 from django.contrib import admin
 
-from product.models import Category, Product
+from product.models import Category, Product, Images
 
+class ProductImageInline(admin.TabularInline):
+    model = Images
+    extra = 5
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['title', 'status']
+    list_display = ['title', 'status', 'image_tag']
+    readonly_fields = ('image_tag',)
     list_filter = ['status']
 
+
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'price', 'gear', 'number_of_seats']
+    list_display = ['title', 'category', 'price', 'image_tag', 'gear', 'number_of_seats']
+    readonly_fields = ('image_tag',)
     list_filter = ['category', 'status']
+    inlines = [ProductImageInline]
+
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'product', 'image_tag']
+    readonly_fields = ('image_tag',)
+
+
 
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Images, ImageAdmin)
--- product/models.py
+++ product/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.safestring import mark_safe
+
 
 class Category (models.Model):
     STATUS = (
@@ -6,7 +8,7 @@ class Category (models.Model):
         ('False', 'Hayır'),
     )
 
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=130)
     description = models.CharField(max_length=255)
     keywords = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
@@ -18,6 +20,10 @@ class Category (models.Model):
 
     def __str__(self):
         return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
 
 class Product(models.Model):
     STATUS = (
@@ -31,7 +37,7 @@ class Product(models.Model):
     )
 
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=150)
     description = models.CharField(max_length=255)
     keywords = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
@@ -49,4 +55,26 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
+
+class Images(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
+
+
+    
+
+        
+
 
