fa95a7fb18f0e94f7c099cd2ed05a89957746c51
miracziyaalev <miracziyaalev@gmail.com>
2021-05-22 01:45:22 +0300

    search button
    i. header`a search butonu eklendi
    ii. products_search html dosyasi olusturuldu
    iii. arama butonu calisir hale getirildi

--- home/views.py
+++ home/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.contrib import messages
 
 # Create your views here.
+from home.forms import SearchForm
 from home.models import Setting, ContactFormu, ContactFormMessage
 from product.models import Product, Category, Images, Comment
 
@@ -72,3 +73,19 @@ def iletisim(request):
     form = ContactFormu()
     context={'setting':setting, 'category': category, 'form':form}
     return render(request, 'iletisim.html', context)
+
+def product_search(request):
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            category = Category.objects.all()
+            query = form.cleaned_data['query']
+            products = Product.objects.filter(title__icontains=query)
+
+            context = {
+                'products' : products,
+                'category' : category,
+            }
+            return render(request, 'products_search.html', context)
+
+    return HttpResponseRedirect('/')
