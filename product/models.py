faaa257c330c4f07eca3fced8f01c5f284904bfb
miracziyaalev <miracziyaalev@gmail.com>
2021-05-22 13:32:48 +0300

    slug maker
    i. otomatik slug olusturma eklendi

--- product/models.py
+++ product/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.forms import ModelForm
+from django.urls import reverse
 from django.utils.safestring import mark_safe
 from ckeditor_uploader.fields import RichTextUploadingField
 from ckeditor.fields import RichTextField
@@ -19,7 +20,7 @@ class Category (MPTTModel):
     keywords = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
     status = models.CharField(max_length=10, choices= STATUS)
-    slug = models.SlugField()
+    slug = models.SlugField(null=False , unique=True)
     parent = TreeForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
@@ -39,6 +40,9 @@ class Category (MPTTModel):
     def image_tag(self):
         return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
     image_tag.short_description = 'Image'
+
+    def get_absolute_url(self):         #oto slug olusturma
+        return reverse('category_detail', kwargs={'slug':self.slug})
 
 class Product(models.Model):
     STATUS = (
@@ -64,7 +68,7 @@ class Product(models.Model):
     engine = models.FloatField(blank=True)
     amount = models.IntegerField(blank=True)
     detail = RichTextUploadingField(blank=True)
-    slug = models.SlugField(max_length=150)
+    slug = models.SlugField(null=False, unique=True)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
@@ -74,6 +78,12 @@ class Product(models.Model):
     def image_tag(self):
         return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
     image_tag.short_description = 'Image'
+
+    def catimg_tag(self):
+        return mark_safe((Category.status))
+
+    def get_absolute_url(self):         #oto slug olusturma
+        return reverse('product_detail', kwargs={'slug':self.slug})
 
 class Images(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
